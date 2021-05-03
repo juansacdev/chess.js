4360aa1b83e78678ccd899288d61517813367f25
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-02 22:43:58 -0500

    create dinamic board

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Board from './class/Board'
-import Piece from './class/Piece';
-import { theme, pieceTheme } from './theme'
+import Pawn from './class/Pieces/Pawn';
+import Rook from './class/Pieces/Rook';
+import Knight from './class/Pieces/Knight';
+import Bishop from './class/Pieces/Bishop';
+import Queen from './class/Pieces/Queen';
+import King from './class/Pieces/King';
+import { theme, pieceTheme } from './utils/theme'
 
 const COLUMNS = 8;
 const FILES = 8;
@@ -8,34 +13,25 @@ const FILES = 8;
 const WIDTH = 800;
 const HEIGHT = 800;
 
-const pieces = {
-	king: ['♚', '♔'],
-	queen: ['♛', '♕'],
-	rook: ['♜', '♖'],
-	bishop: ['♝', '♗'],
-	knight: ['♞', '♘'],
-	pawn: ['♟', '♙'],
-}
-
 const board = new Board (WIDTH, HEIGHT, COLUMNS, FILES, theme, pieceTheme)
 
 // Set all pieces
 for (let x = 0; x < FILES; x++) {
 	// Black pawns
-	board.initPlacePiece(x, 1, new Piece(pieces.pawn, pieceTheme.pieceDark))
+	board.initPlacePiece(x, 1, new Pawn(pieceTheme.pieceDark))
 	// White pawns
-	board.initPlacePiece(x, 6, new Piece(pieces.pawn, pieceTheme.pieceLight))
+	board.initPlacePiece(x, 6, new Pawn(pieceTheme.pieceLight))
 }
 
 for (let x = 0; x < 2; x++) {
-	board.initPlacePiece(0, (x * 7), new Piece(pieces.rook, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
-	board.initPlacePiece(1, (x * 7), new Piece(pieces.knight, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
-	board.initPlacePiece(2, (x * 7), new Piece(pieces.bishop, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
-	board.initPlacePiece(3, (x * 7), new Piece(pieces.queen, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
-	board.initPlacePiece(4, (x * 7), new Piece(pieces.king, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
-	board.initPlacePiece(5, (x * 7), new Piece(pieces.bishop, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
-	board.initPlacePiece(6, (x * 7), new Piece(pieces.knight, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
-	board.initPlacePiece(7, (x * 7), new Piece(pieces.rook, x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(0, (x * 7), new Rook(x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(1, (x * 7), new Knight(x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(2, (x * 7), new Bishop(x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(3, (x * 7), new Queen(x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(4, (x * 7), new King(x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(5, (x * 7), new Bishop(x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(6, (x * 7), new Knight(x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
+	board.initPlacePiece(7, (x * 7), new Rook( x ? pieceTheme.pieceLight : pieceTheme.pieceDark))
 }
 
 board.renderBoard()
