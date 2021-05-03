91ce96ff90c5d557d530424078a1c65ad23b95fb
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-03 04:37:23 -0500

    create movements for rook

--- src/class/Pieces/Rook.js
+++ src/class/Pieces/Rook.js
@@ -5,6 +5,17 @@ class Rook extends Piece {
     constructor(color) {
         super(color, pieceTypes.rook, ['♜', '♖'])
     }
+
+    avalibleMovements(position, matriz) {
+        const possibleMovements = [
+            [0, -1], // Up
+            [0, +1], // Down
+            [+1, 0], // Right
+            [-1, 0], // Left
+        ]
+
+        possibleMovements.forEach(mv => this.checkDirection(position, mv, matriz))
+    }
 }
 
 export default Rook
