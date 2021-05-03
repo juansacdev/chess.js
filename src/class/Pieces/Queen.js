8732110d3700d21fcfb727e9842c50493f4a2a16
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-03 04:48:36 -0500

    cretae movements for queen

--- src/class/Pieces/Queen.js
+++ src/class/Pieces/Queen.js
@@ -5,6 +5,21 @@ class Queen extends Piece {
     constructor(color) {
         super(color, pieceTypes.queen, ['♛', '♕'])
     }
+
+    avalibleMovements(position, matriz) {
+        const possibleMovements = [
+            [0, -1], // Up
+            [0, +1], // Down
+            [+1, 0], // Right
+            [-1, 0], // Left
+            [-1, -1], // Up left
+            [1, -1], // Up right
+            [-1, 1], // Down right
+            [1, 1], // Down left
+        ]
+
+        possibleMovements.forEach(mv => this.checkDirection(position, mv, matriz))
+    }
 }
 
 export default Queen
