10b5ffab8c8451192669d80216904ba961f46c37
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-03 04:57:01 -0500

    create movements for king

--- src/class/Pieces/King.js
+++ src/class/Pieces/King.js
@@ -5,6 +5,30 @@ class King extends Piece {
     constructor(color) {
         super(color, pieceTypes.king, ['♚', '♔'])
     }
+
+    avalibleMovements(position, matriz) {
+        const [x, y] = position
+        const possibleMovements = [
+            [0, -1], // Up
+            [0, +1], // Down
+            [+1, 0], // Right
+            [-1, 0], // Left
+            [-1, -1], // Up left
+            [1, -1], // Up right
+            [-1, 1], // Down right
+            [1, 1], // Down left
+        ]
+
+
+        possibleMovements.forEach(mv =>{
+            const [dirX, dirY] = mv
+            const cell = this.getCellFromCoords([x + (1 * dirX), y + (1 * dirY)], matriz)
+
+            if (cell && !(cell.piece && cell.piece.color === this.color)) {
+                cell.setAvalibeMove(true)
+            }
+        })
+    }
 }
 
 export default King
