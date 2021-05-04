d4ff3b93ba9d21bfa13b7fb106122310ffa86ef3
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-04 08:04:42 -0500

    add header

--- src/helpers/sockets.js
+++ src/helpers/sockets.js
@@ -1,6 +1,10 @@
 export const roomId = window.location.search.split('room=')[1]
 
-const socket = io('https://chessjs-api.herokuapp.com/')
+const socket = io('https://chessjs-api.herokuapp.com/', {
+    extraHeaders: {
+        "Access-Control-Allow-Origin": "*"
+    }
+})
 
 socket.on('connected', () => {
     socket.emit('join room', roomId)
