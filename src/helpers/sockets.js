f550a0e81ae6be7703602e07c5a9dfd84c4ec6f2
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-04 06:57:37 -0500

    update cdn

--- src/helpers/sockets.js
+++ src/helpers/sockets.js
@@ -1,8 +1,6 @@
-import { io } from "socket.io-client";
-
 export const roomId = window.location.search.split('room=')[1]
 
-const socket = io('http://localhost:5000')
+const socket = io('https://chessjs-api.herokuapp.com/')
 
 socket.on('connected', () => {
     socket.emit('join room', roomId)
