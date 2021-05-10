e4f9cb7c958c93249e1d8a70dd1f7ab48c0e5f6d
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-10 02:13:25 -0500

    create random code

---
+++ src/utils/makeCode.js
@@ -0,0 +1,15 @@
+const makeId = (length) => {
+	let result = "";
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(
+			Math.floor(Math.random() * charactersLength),
+		);
+	}
+	return result;
+}
+
+module.exports = {
+	makeId,
+};
